d6cd1c48799f78ddaa3d4d9e2d66ecec812f4759
SprengerV <b.francis@sprengerv.com>
2021-01-16 04:13:19 -0500

    game starts, moves through questions, subtracts time for incorrect answers

--- js/myjava.js
+++ js/myjava.js
@@ -13,11 +13,13 @@ var q4 = ['Which of the following is not a reserved word in JavaScript?','progra
 var qs = [q0, q1, q2, q3, q4];
 var scores = JSON.parse(localStorage.getItem('high scores'));
 var time
+var round
 
 // initialization (on page load)
 function init(){
     msg.innerHTML = 'You have 1 minute to complete the quiz. You lose 1 point for every second you take to answer and 10 points for every question you get wrong.<br>Click START to begin.';
     question.innerHTML = 'CODING QUIZ';
+    question.style.fontSize = '75px';
     answers.style.visibility = 'hidden';
     timer.style.visibility = 'hidden';
     score.innerHTML = getScores();
@@ -37,52 +39,92 @@ function getScores(){
 
 function startGame(){
     time = 60;
-    timer.innerHTML = time; 
+    timer.innerHTML = time;
+    question.style.fontSize = '28px'; 
     start.disabled = true;
     msg.innerHTML = 'Click the answer you think is correct.'
-    displayqs();
-    countdown(time);
-    document.getElementById('timer').style.visibility = 'visible';
-    
-}
-// show the questions/answers
-function displayqs(){
-    for (const q of qs){
-        question.innerHTML = q[0];
-        const correct = q[1];
-        // picks a random number, and switches the correct answer with that node on the list
-        rand = Math.round(Math.random()*3 + 1);
-        console.log(`correct answer should be: ${rand}`);
-        const oddManOut = q[rand];
-        q.splice(rand, 1, q[1]);
-        q[1] = oddManOut;
-        console.log(q);
-        // sets each possible answer in a spot on the page
-        for (i=0;i<4;i++){
-            answers.children[i].innerHTML = q[i+1];
-            answers.style.visibility = 'visible';
-        }
+    // time ticks off
+    var tick = function(){
+        if(time > 0){
+            time--;
+            return time
+        };
+    };
+    setInterval(tick, 1000); 
+    // check for wrong answers
+    setInterval(function(){
+        if(timer.getAttribute('data-pass') == 'wrong'){
+            time -=10;
+            timer.setAttribute('data-pass','notwrong');
+            // return time
+        };
+        if(time <= 0){youLose(tick)};
+    }, 100);
+    // push time to display
+    setInterval(function(){timer.innerHTML = time}, 500);
+    timer.style.visibility = 'visible';
+    answers.style.visibility = 'visible';
+    // TODO for each round play(current){}
+    // start next round
+    round = 0
+    var correct = display(round); 
+    answers.addEventListener('click', function(event){
         
-    }
-}
-// timer
-function countdown(time){
-    var tick = setInterval(function(){
-        time--;
-        timer.innerHTML = time; 
-        if (time <= 0) {
-            youLose();
+        if(round<qs.length+1){
+            if (event.target !== correct){
+                timer.setAttribute('data-pass','wrong');
+                round++
+                display(round);    
+            }; 
+            if(event.target == correct){
+                round++
+                display(round);
+            };
+        } else{
             clearInterval(tick);
-        }
-    }, 1000);
-}
+            youScored(tick);
+        };
+    });   
+};
+
+
+// display a round on screen
+function display(round){
+    console.log(`round: ${round}`);
+    // current set/question
+    var current = qs[round];
+    var rand = Math.round(Math.random()*3 + 1);
+    console.log(`correct answer should be: ${rand}`);
+    // swap correct answer (node 1) into random spot
+    const oddManOut = current[rand];
+    current.splice(rand, 1, current[1]);
+    current[1] = oddManOut;
+    console.log(current);
+    // push question and answers to page
+    question.innerHTML = current[0];
+    for (i=1;i<current.length;i++){
+        let ans = current[i];
+        answers.children[i-1].textContent = ans;
+    };
+    console.log(current[rand]);
+    return current[rand];
+ 
+    // jumble answers so the correct one isn't in the same place everytime
+    // picks a random number, and switches the correct answer with that node on the list
+
+};
 // if clock reaches 0
 function youLose(){
     answers.style.visibility = 'hidden';
     question.innerHTML = 'GAME OVER';
+    question.style.fontSize = '75px';
     msg.innerHTML = 'You can\'t make the leaderboard with no points. <br>Click START to try again.';
     start.disabled = false;
-}
+    start.innerHTML = 'RETRY'
+};
+function endGame(){
+    init();
+};
 
 window.onload = init();
 
